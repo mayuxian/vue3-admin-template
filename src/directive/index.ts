c1a7a1b8048e9c939ec9141d84781b6272e4440e
mayuxian <mayuxian@100doc.com.cn>
2022-10-12 15:44:55 +0800

    完善加载指令模块

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -3,7 +3,10 @@ const modules = import.meta.globEager('./**/*.[t|j]s')
 let directiveConfig: any = []
 Object.keys(modules).forEach(key => {
   if (key === './index.ts') return
-  directiveConfig = directiveConfig.concat(modules[key].default)
+  const module = modules[key].default
+  if (module.install) {
+    directiveConfig = directiveConfig.concat(modules[key].default)
+  }
 })
 
 // export default directiveConfig
