d055efaf6b6d6799a774ca6208fce886b29a8b9a
mayuxian <mayuxian830@163.com>
2024-11-21 15:10:23 +0800

    备注TODO

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { createPinia } from 'pinia'
 const store = createPinia()
+//TODO:后续需要换成 pinia-plugin-persistedstate ，pinia-plugin-persist已不维护。modules的方式和storage都要修改下
 import piniaPluginPersist from 'pinia-plugin-persist'
 store.use(piniaPluginPersist)
 export default store
